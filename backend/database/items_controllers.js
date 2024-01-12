08cbd9325e6a074782bd932aad1ec33331bcf927
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-01-12 18:42:40 +0530

    Added Item Table and made controller

---
+++ backend/database/items_controllers.js
@@ -0,0 +1,55 @@
+import mysql from "mysql2";
+
+const pool = mysql
+  .createPool({
+    host: "127.0.0.1",
+    user: "root",
+    password: "123",
+    database: "foodiee",
+  })
+  .promise();
+
+export async function addItem(
+  ShopkeeperID,
+  ItemName,
+  Description,
+  Price,
+  ImageURL,
+  ExpectedTime,
+  CousineType
+) {
+  let sql =
+    "INSERT INTO Items (ShopkeeperID, ItemName, Description, Price, ImageURL, ExpectedTime, CousineType) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  let data = [
+    ShopkeeperID,
+    ItemName,
+    Description,
+    Price,
+    ImageURL,
+    ExpectedTime,
+    CousineType,
+  ];
+
+  try {
+    const [row] = await pool.query(sql, data);
+    // const result = await showItem(row.insertId);
+    console.log(`Item added successfully with ID ${row.insertId}`)
+    return row;
+  } catch (error) {
+    console.error(`An error occurred while adding the item: ${error}`);
+    throw error;
+  }
+}
+
+export async function showItem(ItemID) {
+  try {
+    const [row] = await pool.query("SELECT * FROM Items WHERE ItemID = ?", [
+      ItemID,
+    ]);
+    return row[0];
+  } catch (error) {
+    console.error(`An error occurred while adding the item: ${error}`);
+    throw error;
+  }
+}
+
---
+++ backend/models/items_scheman.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Items (
+    ItemID INT AUTO_INCREMENT PRIMARY KEY,
+    ShopkeeperID INT,
+    ItemName VARCHAR(255),
+    Description TEXT,
+    Price DECIMAL(10, 2),
+    ImageURL VARCHAR(255),
+    ExpectedTime VARCHAR(255),
+    CousineType VARCHAR(255),
+    Created TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+
+ALTER TABLE Items CHANGE ShopkeepersID ShopkeeperID INT;
+
+ALTER TABLE Items MODIFY ItemID INT AUTO_INCREMENT PRIMARY KEY;
